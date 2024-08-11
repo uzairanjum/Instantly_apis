578d0cb09142bae8d9ac61be1679b98fe3f46ab1
Uzair Anjum <rajpoot.waryah@gmail.com>
2024-08-11 14:01:37 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-from typing import Dict, Union
-from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 
@@ -15,5 +13,11 @@ def root():
 @app.post('/gepeto/incoming-sms', tags=['Webhook'], summary="Incoming sms webhook")
 def incoming_sms(sms:dict):
     print("------------webhook,",sms)
+    return JSONResponse(content={"status": "success"}, status_code=200)
+
+
+@app.post('/gepeto/case-webhook', tags=['Webhook'], summary="salesforce webhook")
+def salesforce_webhook(data:dict):
+    print("------------webhook,",data)
     return JSONResponse(content={"status": "success"}, status_code=200)
 
