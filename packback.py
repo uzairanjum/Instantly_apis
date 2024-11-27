3155ef521178c4f70defb96c484d63c97aa5d9bd
Uzair Anjum <rajpoot.waryah@gmail.com>
2024-11-28 01:40:53 +0500

    Update packback.py

--- packback.py
+++ packback.py
@@ -4,7 +4,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 import time
 from src.common.logger import get_logger
 
-from packback_csv import process_csv_concurrent
+from packback_csv import process_csv_with_concurrency
 
 
 
@@ -20,7 +20,7 @@ def packback_lead_course():
     try:
         start_time = time.time()  # Start time
         logger.info("packback_lead_course is running")
-        process_csv_concurrent()
+        process_csv_with_concurrency()
         elapsed_time = time.time() - start_time  # Calculate elapsed time
 
         logger.info("packback_lead_course is completed in %.2f seconds", elapsed_time)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     try:
         scheduler = BackgroundScheduler()
         logger.info("packback scheduler is running")
-        scheduler.add_job(process_csv_concurrent, 'interval', minutes=15)
+        scheduler.add_job(process_csv_with_concurrency, 'interval', minutes=15)
         scheduler.start()
         while True:
             pass
